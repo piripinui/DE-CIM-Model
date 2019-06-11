675716d3f7467fb915a144cda5156122ad4f0acf
piripinui <philliprobinson@US-C02TW1DNHV2Q.local>
2019-06-11 12:59:58 +0100

    Positioned files correctly

--- schema-server/server.js
+++ schema-server/server.js
@@ -5,6 +5,6 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-	console.log('schema-server listening on port ' + 3000 + '!');
+app.listen(80, function () {
+	console.log('schema-server listening on port ' + 80 + '!');
 });
